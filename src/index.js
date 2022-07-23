30e8d1e8c3d911c75655df6b4025f0e263564437
Konstantin Budylin <hts7808@gmail.com>
2022-07-23 21:57:39 +0300

    add user context

--- src/index.js
+++ src/index.js
@@ -4,15 +4,16 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 import Routes from 'routes'
 import TopBar from 'components/topbar'
+import { CurrentUserProvider } from 'contexts/currentUser'
 
 const App = () => {
   return (
-    <div className="App">
+    <CurrentUserProvider>
       <Router>
         <TopBar />
         <Routes />
       </Router>
-    </div>
+    </CurrentUserProvider>
   )
 }
 
